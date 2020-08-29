d81e741c5528d1361c968416eb24804e4a7f63e7
Abinash Panda <abinash@prodios.com>
2020-08-29 14:15:45 +0530

    Add more tests for button

--- src/components/button/button.test.tsx
+++ src/components/button/button.test.tsx
@@ -1,8 +1,50 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Button from './button'
 
 test('render button label correctly', () => {
   render(<Button label="Click Here" />)
   expect(screen.getByText('Click Here')).toBeInTheDocument()
+})
+
+test('renders primary button correctly', () => {
+  render(<Button label="Click Here" buttonType={Button.ButtonType.primary} />)
+  expect(
+    screen.getByLabelText('Click Here').classList.contains('primary'),
+  ).toBe(true)
+})
+
+test('renders default button correctly', () => {
+  render(<Button label="Click Here" buttonType={Button.ButtonType.default} />)
+  expect(
+    screen.getByLabelText('Click Here').classList.contains('default'),
+  ).toBe(true)
+})
+
+test('renders danger button correctly', () => {
+  render(<Button label="Click Here" buttonType={Button.ButtonType.danger} />)
+  expect(screen.getByLabelText('Click Here').classList.contains('danger')).toBe(
+    true,
+  )
+})
+
+test('renders link button correctly', () => {
+  render(<Button label="Click Here" buttonType={Button.ButtonType.link} />)
+  expect(screen.getByLabelText('Click Here').classList.contains('link')).toBe(
+    true,
+  )
+})
+
+test('render icon correctly', () => {
+  const icon = <div className="icon" data-testid="icon" />
+  render(<Button label="Click Here" icon={icon} />)
+  expect(screen.getByTestId('icon')).toBeInTheDocument()
+})
+
+test('call onClick on button click', () => {
+  const onClick = jest.fn()
+  render(<Button label="Click Here" onClick={onClick} />)
+  userEvent.click(screen.getByText('Click Here'))
+  expect(onClick).toHaveBeenCalled()
 })
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -39,12 +39,12 @@ export default function Button({
         'py-2 rounded-md focus:outline-none focus:shadow-outline border text-sm font-medium flex items-center space-x-2 transition-colors duration-300',
         iconOnlyButton ? 'px-2' : 'px-3',
         buttonType === ButtonType.primary
-          ? 'bg-blue-600 text-white border-transparent'
+          ? 'bg-blue-600 text-white border-transparent primary'
           : buttonType === ButtonType.default
-          ? 'border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white'
+          ? 'border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white default'
           : buttonType === ButtonType.danger
-          ? 'border-red-500 text-red-500 hover:bg-red-500 hover:text-white'
-          : 'border-transparent text-blue-600',
+          ? 'border-red-500 text-red-500 hover:bg-red-500 hover:text-white danger'
+          : 'border-transparent text-blue-600 link',
         className,
       )}
       style={style}
