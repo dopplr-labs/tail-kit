f41c0c02e31f245a83739efe7828bf028f153f4e
Abinash Panda <abinash@prodios.com>
2020-08-29 12:51:55 +0530

    Add open and close transition in modal

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,7 +1,8 @@
-import React, { useRef, useLayoutEffect } from 'react'
+import React, { useRef, useCallback } from 'react'
+import { useMemoOne } from 'use-memo-one'
 import Button, { Props as ButtonProps } from 'components/button'
 import { createPortal } from 'react-dom'
-import clsx from 'clsx'
+import { CSSTransition } from 'react-transition-group'
 
 type ActionButtonProps = Omit<ButtonProps, 'onClick'>
 
@@ -30,66 +31,76 @@ export default function Modal({
   actions,
   portalParent = document.body,
 }: Props) {
-  // TODO: Replace useRef -> useMemoOne
-  const portalContainer = useRef(document.createElement('div')).current
-  useLayoutEffect(() => {
-    portalContainer.classList.add('modal-portal-container')
-    portalParent.appendChild(portalContainer)
-    return () => {
-      portalParent.removeChild(portalContainer)
-    }
-  }, [portalContainer, portalParent])
+  const portalContainer = useMemoOne(() => {
+    const container = document.createElement('div')
+    container.classList.add('modal-portal-container')
+    return container
+  }, [])
 
   const contentContainer = useRef<HTMLDivElement | null>(null)
 
+  const handleOverlayClick = useCallback(
+    (event) => {
+      event.stopPropagation()
+      if (!contentContainer.current?.contains(event.target as HTMLElement)) {
+        onRequestClose?.()
+      }
+    },
+    [onRequestClose],
+  )
+
   return createPortal(
-    <div
-      className={clsx(
-        'modal-overlay fixed inset-0 bg-opacity-75 bg-black p-4 flex items-center justify-center transition-all duration-300',
-        visible
-          ? 'opacity-100 pointer-events-auto'
-          : 'opacity-0 pointer-events-none',
-      )}
-      onClick={(event) => {
-        if (!contentContainer.current?.contains(event.target as HTMLElement)) {
-          onRequestClose?.()
-        }
+    <CSSTransition
+      in={visible}
+      timeout={300}
+      classNames="modal"
+      unmountOnExit
+      onEnter={() => {
+        portalParent.appendChild(portalContainer)
+      }}
+      onExited={() => {
+        portalParent.removeChild(portalContainer)
       }}
     >
       <div
-        className="flex flex-col w-full max-w-screen-sm max-h-full overflow-hidden bg-white rounded-md shadow-2xl modal-content-container"
-        ref={contentContainer}
+        className="fixed inset-0 flex items-center justify-center p-4 bg-black bg-opacity-75 modal-overlay"
+        onClick={handleOverlayClick}
       >
-        {title ? (
-          <div className="px-4 py-3 text-lg font-semibold text-gray-900 border-b">
-            {title}
+        <div
+          className="flex flex-col w-full max-w-screen-sm max-h-full overflow-hidden bg-white rounded-md shadow-2xl modal-content-container"
+          ref={contentContainer}
+        >
+          {title ? (
+            <div className="px-4 py-3 text-lg font-semibold text-gray-900 border-b">
+              {title}
+            </div>
+          ) : null}
+          <div className="flex-1 p-4 overflow-auto text-sm text-gray-700 border-b modal-content-container">
+            {children}
           </div>
-        ) : null}
-        <div className="flex-1 p-4 overflow-auto text-sm text-gray-700 border-b modal-content-container">
-          {children}
-        </div>
-        <div className="flex items-center justify-end px-4 py-3 space-x-4">
-          {actions ?? (
-            <>
-              <Button
-                label="Cancel"
-                buttonType={Button.ButtonType.default}
-                onClick={onCancel ?? onRequestClose}
-                tabIndex={visible ? 0 : -1}
-                {...cancelButtonProps}
-              />
-              <Button
-                label="OK"
-                buttonType={Button.ButtonType.primary}
-                onClick={onOK ?? onRequestClose}
-                tabIndex={visible ? 0 : -1}
-                {...okButtonProps}
-              />
-            </>
-          )}
+          <div className="flex items-center justify-end px-4 py-3 space-x-4">
+            {actions ?? (
+              <>
+                <Button
+                  label="Cancel"
+                  buttonType={Button.ButtonType.default}
+                  onClick={onCancel ?? onRequestClose}
+                  tabIndex={visible ? 0 : -1}
+                  {...cancelButtonProps}
+                />
+                <Button
+                  label="OK"
+                  buttonType={Button.ButtonType.primary}
+                  onClick={onOK ?? onRequestClose}
+                  tabIndex={visible ? 0 : -1}
+                  {...okButtonProps}
+                />
+              </>
+            )}
+          </div>
         </div>
       </div>
-    </div>,
+    </CSSTransition>,
     portalContainer,
   )
 }
