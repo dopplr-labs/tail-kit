044e33762c0bd691bf0f7d104d9815e26bdb9482
Abinash Panda <abinash@prodios.com>
2020-09-02 11:01:01 +0530

    Add forwardRef to Button and Input components

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import clsx from 'clsx'
 
 export enum ButtonType {
@@ -30,38 +30,46 @@ export type ButtonProps = Omit<
   style?: React.CSSProperties
 }
 
-export function Button({
-  label,
-  icon,
-  buttonType = ButtonType.default,
-  className,
-  style,
-  ...restProps
-}: ButtonProps) {
-  const iconOnlyButton = icon && !label
+const ButtonComponent = forwardRef(
+  (
+    {
+      label,
+      icon,
+      buttonType = ButtonType.default,
+      className,
+      style,
+      ...restProps
+    }: ButtonProps,
+    ref: React.Ref<HTMLButtonElement>,
+  ) => {
+    const iconOnlyButton = icon && !label
 
-  return (
-    <button
-      aria-label={label || restProps['aria-label']}
-      className={clsx(
-        'py-2 rounded-md focus:outline-none focus:shadow-outline border text-sm font-medium flex items-center justify-center space-x-2 transition-colors duration-300',
-        iconOnlyButton ? 'px-2' : 'px-3',
-        buttonType === ButtonType.primary
-          ? 'bg-blue-600 text-white border-transparent primary'
-          : buttonType === ButtonType.default
-          ? 'border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white default'
-          : buttonType === ButtonType.danger
-          ? 'border-red-500 text-red-500 hover:bg-red-500 hover:text-white danger'
-          : 'border-transparent text-blue-600 link',
-        className,
-      )}
-      style={style}
-      {...restProps}
-    >
-      {icon}
-      {label ? <span className="text-center">{label}</span> : null}
-    </button>
-  )
-}
+    return (
+      <button
+        aria-label={label || restProps['aria-label']}
+        className={clsx(
+          'py-2 rounded-md focus:outline-none focus:shadow-outline border text-sm font-medium flex items-center justify-center space-x-2 transition-colors duration-300',
+          iconOnlyButton ? 'px-2' : 'px-3',
+          buttonType === ButtonType.primary
+            ? 'bg-blue-600 text-white border-transparent primary'
+            : buttonType === ButtonType.default
+            ? 'border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white default'
+            : buttonType === ButtonType.danger
+            ? 'border-red-500 text-red-500 hover:bg-red-500 hover:text-white danger'
+            : 'border-transparent text-blue-600 link',
+          className,
+        )}
+        style={style}
+        {...restProps}
+        ref={ref}
+      >
+        {icon}
+        {label ? <span className="text-center">{label}</span> : null}
+      </button>
+    )
+  },
+)
 
-Button.ButtonType = ButtonType
+ButtonComponent.displayName = 'Button'
+
+export const Button = Object.assign(ButtonComponent, { ButtonType })
--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,4 +1,4 @@
-import React, { cloneElement } from 'react'
+import React, { cloneElement, forwardRef } from 'react'
 import clsx from 'clsx'
 
 export type InputProps = React.DetailedHTMLProps<
@@ -10,31 +10,39 @@ export type InputProps = React.DetailedHTMLProps<
   inputStyle?: React.CSSProperties
 }
 
-export function Input({
-  icon,
-  className,
-  style,
-  inputClassName,
-  inputStyle,
-  ...restProps
-}: InputProps) {
-  return (
-    <div
-      className={clsx(
-        'px-3 py-2 focus-within:shadow-outline rounded-md border flex items-center space-x-3 text-gray-400',
-        className,
-      )}
-      style={style}
-    >
-      {icon ? cloneElement(icon, { className: 'w-5 h-5' }) : null}
-      <input
+export const Input = forwardRef(
+  (
+    {
+      icon,
+      className,
+      style,
+      inputClassName,
+      inputStyle,
+      ...restProps
+    }: InputProps,
+    ref: React.Ref<HTMLInputElement>,
+  ) => {
+    return (
+      <div
         className={clsx(
-          'text-sm font-sans text-gray-800 placeholder-gray-400 flex-1 focus:outline-none',
-          inputClassName,
+          'px-3 py-2 focus-within:shadow-outline rounded-md border flex items-center space-x-3 text-gray-400',
+          className,
         )}
-        style={inputStyle}
-        {...restProps}
-      />
-    </div>
-  )
-}
+        style={style}
+      >
+        {icon ? cloneElement(icon, { className: 'w-5 h-5' }) : null}
+        <input
+          className={clsx(
+            'text-sm font-sans text-gray-800 placeholder-gray-400 flex-1 focus:outline-none',
+            inputClassName,
+          )}
+          style={inputStyle}
+          {...restProps}
+          ref={ref}
+        />
+      </div>
+    )
+  },
+)
+
+Input.displayName = 'Input'
