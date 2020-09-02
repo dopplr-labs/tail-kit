caa19e58ed3c43aa50909e72283e3b1449e1f964
Abinash Panda <abinash@prodios.com>
2020-09-02 11:11:53 +0530

    Initial commit of tests for Input component

--- src/components/input/input.test.tsx
+++ src/components/input/input.test.tsx
@@ -1,1 +1,21 @@
-test('render input correctly', () => {})
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { Input } from './input'
+
+test('render input correctly', () => {
+  render(<Input placeholder="Email" />)
+  expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
+})
+
+test('render icon correctly', () => {
+  const icon = <div className="icon" data-testid="icon" />
+  render(<Input placeholder="Email" icon={icon} />)
+  expect(screen.getByTestId('icon')).toBeInTheDocument()
+})
+
+test('fowards ref correctly to input element', () => {
+  const ref = React.createRef<HTMLInputElement>()
+  render(<Input placeholder="Email" ref={ref} />)
+  expect(ref.current?.tagName).toBe('INPUT')
+  expect(ref.current?.placeholder).toMatch(/email/i)
+})
