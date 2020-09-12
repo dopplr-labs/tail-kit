47ab6ffa1d5393e3ae73ff84ca9a9c8a05428c6c
hnagrath09 <him.nagrath@gmail.com>
2020-09-12 10:44:55 +0530

    [ref-#5]Added onChange function

--- src/components/checkbox/checkbox-group.tsx
+++ src/components/checkbox/checkbox-group.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Checkbox } from './checkbox'
 
 export type OptionType = {
-  label?: string
+  label: string
   value?: string
   disabled?: boolean
 }
@@ -11,12 +11,14 @@ export type CheckboxGroupProps = {
   options: OptionType[] | string[]
   defaultValue?: string[]
   disabled?: boolean
+  onChange?: (checkedValues: string[]) => void
 }
 
 export function CheckboxGroup({
   options,
   defaultValue,
   disabled,
+  onChange,
 }: CheckboxGroupProps) {
   function getOptions() {
     return (options as Array<OptionType>).map((option) => {
@@ -28,18 +30,30 @@ export function CheckboxGroup({
     })
   }
   const optionProp = getOptions()
+  const [checkedItem, setCheckedItem] = useState(
+    optionProp.map((option) => defaultValue?.indexOf(option.label) !== -1),
+  )
 
   return (
     <div className="space-x-8">
-      {optionProp?.map((option: any) => (
+      {optionProp?.map((option: any, index) => (
         <Checkbox
           key={option.label}
           label={option.label}
           value={option?.value}
           disabled={option?.disabled ?? disabled}
-          defaultChecked={
-            !!defaultValue?.find((value) => option.label === value)
-          }
+          checked={checkedItem[index]}
+          onChange={() => {
+            setCheckedItem((prevState) =>
+              prevState.map((item, id) => (id === index ? !item : item)),
+            )
+            if (onChange) {
+              const checkedValues = optionProp
+                .filter((option, index) => checkedItem[index])
+                .map((option) => option.label)
+              onChange(checkedValues)
+            }
+          }}
         />
       ))}
     </div>
