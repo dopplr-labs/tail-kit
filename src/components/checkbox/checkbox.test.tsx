8e48764dec206c5f27dc1c195cba4b6980b3d7a4
hnagrath09 <him.nagrath@gmail.com>
2020-09-12 21:55:31 +0530

    [ref-#5] Added two test cases

--- src/components/checkbox/checkbox.test.tsx
+++ src/components/checkbox/checkbox.test.tsx
@@ -1,1 +1,30 @@
-test('renders checkbox correctly', () => {})
+import React from 'react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { Checkbox } from './checkbox'
+
+test('renders checkbox label correctly', () => {
+  let checked = false
+  render(
+    <Checkbox
+      label="Checkbox label"
+      checked={checked}
+      onChange={() => {
+        checked = !checked
+      }}
+    />,
+  )
+  expect(screen.getByText('Checkbox label')).toBeInTheDocument()
+})
+
+test('onChange event of Checkbox working correctly', () => {
+  let checked = false
+  render(
+    <Checkbox
+      label="Checkbox label"
+      checked={checked}
+      onChange={() => (checked = !checked)}
+    />,
+  )
+  fireEvent.click(screen.getByText('Checkbox label'))
+  expect(checked).toBe(true)
+})
