f5fb7a96ca6c543f5b3528d1cc787ea6edbc8164
hnagrath09 <him.nagrath@gmail.com>
2020-09-25 17:50:16 +0530

    Added test cases for input-number component

--- src/components/input-number/input-number.test.tsx
+++ src/components/input-number/input-number.test.tsx
@@ -1,1 +1,30 @@
-test('renders InputNumber component correctly', () => {})
+import React from 'react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { InputNumber } from './input-number'
+
+test('renders InputNumber component correctly', () => {
+  render(<InputNumber defaultValue={3.5} />)
+  expect(screen.getByDisplayValue('3.5')).toBeInTheDocument()
+})
+
+test('increment button working correctly', () => {
+  render(<InputNumber defaultValue={1} step={0.01} precision={3} />)
+  fireEvent.click(screen.getAllByRole('button')[0])
+  expect(screen.getByTestId('input-number')).toHaveValue(1.01)
+})
+
+test('decrement button working correctly', () => {
+  render(<InputNumber defaultValue={1} step={0.01} precision={3} />)
+  fireEvent.click(screen.getAllByRole('button')[1])
+  expect(screen.getByTestId('input-number')).toHaveValue(0.99)
+})
+
+test('onChange of input working correctly', () => {
+  const onChange = jest.fn((event) => event.target.value)
+  render(<InputNumber min={0} max={10} onChange={onChange} />)
+  userEvent.type(screen.getByTestId('input-number'), '1.25')
+  expect(onChange).toBeCalled()
+  expect(onChange.mock.results[3].value).toBe('1.25')
+  expect(screen.getByTestId('input-number')).toHaveValue(1.25)
+})
