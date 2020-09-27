8d4a9683890c83d0a4c4dbdc00e3ed5b97aa7e6c
hnagrath09 <him.nagrath@gmail.com>
2020-09-27 22:37:28 +0530

    Added new story to show icon in list items

--- src/components/select/select.stories.tsx
+++ src/components/select/select.stories.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import { Meta } from '@storybook/react/types-6-0'
-import { Select } from './select'
+import { action } from '@storybook/addon-actions'
+import {
+  BeakerOutline,
+  CalculatorOutline,
+  ChipOutline,
+  ClipboardListOutline,
+} from 'components/icons'
+import { optionType, Select } from './select'
 
 export default {
   title: 'Data Entry/Select',
@@ -16,5 +23,48 @@ export function DefaultSelect() {
     'Doctor Strange',
     'Hawk Eye',
   ]
-  return <Select label="Select Avenger" options={options} className="w-48" />
+  function handleChange(selectedItem: string | optionType | null) {
+    action('selected option')(selectedItem)
+  }
+  return (
+    <Select
+      placeholder="Select Avenger"
+      options={options}
+      onChange={handleChange}
+      className="w-48"
+    />
+  )
+}
+
+export function DisabledSelect() {
+  const options = [
+    'Black Widow',
+    'Hulk',
+    'Spiderman',
+    'Captain America',
+    'Doctor Strange',
+    'Hawk Eye',
+  ]
+  return (
+    <Select
+      placeholder="Select Avenger"
+      options={options}
+      disabled
+      className="w-48"
+    />
+  )
+}
+
+export function SelectWithListIcons() {
+  const options = [
+    { label: 'Beaker', value: 'beaker', icon: <BeakerOutline /> },
+    { label: 'Calculator', value: 'calculator', icon: <CalculatorOutline /> },
+    { label: 'Chip', value: 'chip', icon: <ChipOutline /> },
+    {
+      label: 'Clipboard List',
+      value: 'clipboard-list',
+      icon: <ClipboardListOutline />,
+    },
+  ]
+  return <Select options={options} placeholder="Select SVG" className="w-56" />
 }
