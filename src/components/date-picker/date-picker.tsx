ea541515269d57d49edbf0c3f06cc398404822ba
Abinash Panda <abinash@prodios.com>
2020-09-27 22:37:28 +0530

    Add Portal in DatePicker component

--- src/components/date-picker/date-picker.tsx
+++ src/components/date-picker/date-picker.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import {
   CalendarOutline,
   ChevronDoubleLeftOutline,
@@ -10,6 +10,9 @@ import {
 import dayjs from 'dayjs'
 import clsx from 'clsx'
 import { CSSTransition, SwitchTransition } from 'react-transition-group'
+import useOutsideClick from 'hooks/use-outside-click'
+import { useMemoOne } from 'use-memo-one'
+import Portal from 'components/portal'
 
 export type DatePickerProps = {
   /** date selected */
@@ -65,9 +68,29 @@ export function DatePicker({
 
   const monthDates = getMonthDates(activeDate)
 
+  const [calendarVisible, setCalendarVisible] = useState(false)
+
+  const triggerContainer = useRef<HTMLDivElement | null>(null)
+
+  const calendarContainer = useRef<HTMLDivElement | null>(null)
+
+  useOutsideClick({
+    containers: useMemoOne(() => [triggerContainer, calendarContainer], []),
+    active: calendarVisible,
+    onClick: () => {
+      setCalendarVisible(false)
+    },
+  })
+
   return (
     <div style={style} className={className}>
-      <div className="flex items-center px-3 py-2 space-x-3 text-gray-400 border rounded-md">
+      <div
+        className="flex items-center px-3 py-2 space-x-3 text-gray-400 border rounded-md"
+        ref={triggerContainer}
+        onClick={() => {
+          setCalendarVisible((prevState) => !prevState)
+        }}
+      >
         {date ? (
           <span className="flex-1 text-sm text-gray-400">
             {dayjs(date).format('DD MMM YYYY')}
@@ -90,131 +113,136 @@ export function DatePicker({
           <CalendarOutline className="w-5 h-5" />
         )}
       </div>
-      <div className="rounded shadow w-80">
-        <div className="flex items-center px-4 py-3 space-x-2 text-gray-400 transition-colors duration-75 ease-in border-b hover:text-gray-800">
-          <button
-            className="rounded focus:outline-none focus:shadow-outline"
-            onClick={() => {
-              setActiveDate((prevState) =>
-                dayjs(prevState).subtract(1, 'year').toDate(),
-              )
-            }}
-          >
-            <ChevronDoubleLeftOutline className="w-5 h-5" />
-          </button>
-          <button
-            className="rounded focus:outline-none focus:shadow-outline"
-            onClick={() => {
-              setActiveDate((prevState) =>
-                dayjs(prevState).subtract(1, 'month').toDate(),
-              )
-            }}
-          >
-            <ChevronLeftOutline className="w-5 h-5" />
-          </button>
-          <span
-            className="flex-1 text-sm font-medium text-center text-gray-800"
-            onClick={() => {
-              setActiveDate((prevState) =>
-                dayjs(prevState).add(1, 'month').toDate(),
-              )
-            }}
-          >
-            {dayjs(activeDate).format('MMM YYYY')}
-          </span>
-          <button
-            className="rounded focus:outline-none focus:shadow-outline"
-            onClick={() => {
-              setActiveDate((prevState) =>
-                dayjs(prevState).add(1, 'month').toDate(),
-              )
-            }}
-          >
-            <ChevronRightOutline className="w-5 h-5" />
-          </button>
-          <button
-            className="rounded focus:outline-none focus:shadow-outline"
-            onClick={() => {
-              setActiveDate((prevState) =>
-                dayjs(prevState).add(1, 'year').toDate(),
-              )
-            }}
-          >
-            <ChevronDoubleRightOutline className="w-5 h-5" />
-          </button>
-        </div>
-
-        <div
-          className="transition-all duration-100 ease-in"
-          style={{ height: containerHeight }}
-        >
-          <SwitchTransition mode="out-in">
-            <CSSTransition
-              key={dayjs(activeDate).valueOf()}
-              classNames="date-picker"
-              addEndListener={(node, done) => {
-                node.addEventListener('transitionend', done, false)
-              }}
-              onEnter={(node: HTMLElement) => {
-                setContainerHeight(node.offsetHeight)
+      <Portal triggerRef={triggerContainer} visible={calendarVisible}>
+        <div className="bg-white rounded shadow w-80" ref={calendarContainer}>
+          <div className="flex items-center px-4 py-3 space-x-2 text-gray-400 transition-colors duration-75 ease-in border-b hover:text-gray-800">
+            <button
+              className="rounded focus:outline-none focus:shadow-outline"
+              onClick={() => {
+                setActiveDate((prevState) =>
+                  dayjs(prevState).subtract(1, 'year').toDate(),
+                )
               }}
             >
-              <div className="px-4 py-3 border-b">
-                <div className="flex items-center mb-2 space-x-1">
-                  {days.map((day) => (
-                    <div
-                      key={day}
-                      className="flex-1 text-sm font-semibold text-gray-900"
-                    >
-                      {day}
-                    </div>
-                  ))}
-                </div>
+              <ChevronDoubleLeftOutline className="w-5 h-5" />
+            </button>
+            <button
+              className="rounded focus:outline-none focus:shadow-outline"
+              onClick={() => {
+                setActiveDate((prevState) =>
+                  dayjs(prevState).subtract(1, 'month').toDate(),
+                )
+              }}
+            >
+              <ChevronLeftOutline className="w-5 h-5" />
+            </button>
+            <span
+              className="flex-1 text-sm font-medium text-center text-gray-800"
+              onClick={() => {
+                setActiveDate((prevState) =>
+                  dayjs(prevState).add(1, 'month').toDate(),
+                )
+              }}
+            >
+              {dayjs(activeDate).format('MMM YYYY')}
+            </span>
+            <button
+              className="rounded focus:outline-none focus:shadow-outline"
+              onClick={() => {
+                setActiveDate((prevState) =>
+                  dayjs(prevState).add(1, 'month').toDate(),
+                )
+              }}
+            >
+              <ChevronRightOutline className="w-5 h-5" />
+            </button>
+            <button
+              className="rounded focus:outline-none focus:shadow-outline"
+              onClick={() => {
+                setActiveDate((prevState) =>
+                  dayjs(prevState).add(1, 'year').toDate(),
+                )
+              }}
+            >
+              <ChevronDoubleRightOutline className="w-5 h-5" />
+            </button>
+          </div>
 
-                <div className="space-y-2">
-                  {monthDates.map((weekDays) => (
-                    <div
-                      className="flex items-center space-x-1"
-                      key={dayjs(weekDays[0]).valueOf()}
-                    >
-                      {weekDays.map((day) => (
-                        <button
-                          key={dayjs(day).valueOf()}
-                          className={clsx(
-                            'flex-1 text-sm focus:outline-none focus:shadow-outline rounded',
-                            date && dayjs(date).valueOf() === day.valueOf()
-                              ? 'text-blue-600 hover:bg-blue-600 hover:text-white font-medium bg-blue-50'
-                              : dayjs(day).format('MM YY') ===
-                                dayjs(activeDate).format('MM YY')
-                              ? 'text-gray-600 hover:bg-gray-100'
-                              : 'text-gray-300 hover:bg-gray-100',
-                          )}
-                          onClick={() => {
-                            setActiveDate(dayjs(day).startOf('month').toDate())
-                            if (onChange) {
-                              onChange(day)
-                            }
-                          }}
-                        >
-                          {dayjs(day).format('DD')}
-                        </button>
-                      ))}
-                    </div>
-                  ))}
+          <div
+            className="transition-all duration-100 ease-in"
+            style={{ height: containerHeight }}
+          >
+            <SwitchTransition mode="out-in">
+              <CSSTransition
+                key={dayjs(activeDate).valueOf()}
+                classNames="date-picker"
+                addEndListener={(node, done) => {
+                  node.addEventListener('transitionend', done, false)
+                }}
+                onEnter={(node: HTMLElement) => {
+                  setContainerHeight(node.offsetHeight)
+                }}
+              >
+                <div className="px-4 py-3 border-b">
+                  <div className="flex items-center mb-2 space-x-1">
+                    {days.map((day) => (
+                      <div
+                        key={day}
+                        className="flex-1 text-sm font-semibold text-gray-900"
+                      >
+                        {day}
+                      </div>
+                    ))}
+                  </div>
+
+                  <div className="space-y-2">
+                    {monthDates.map((weekDays) => (
+                      <div
+                        className="flex items-center space-x-1"
+                        key={dayjs(weekDays[0]).valueOf()}
+                      >
+                        {weekDays.map((day) => (
+                          <button
+                            key={dayjs(day).valueOf()}
+                            className={clsx(
+                              'flex-1 text-sm focus:outline-none focus:shadow-outline rounded',
+                              date && dayjs(date).valueOf() === day.valueOf()
+                                ? 'text-blue-600 hover:bg-blue-600 hover:text-white font-medium bg-blue-50'
+                                : dayjs(day).format('MM YY') ===
+                                  dayjs(activeDate).format('MM YY')
+                                ? 'text-gray-600 hover:bg-gray-100'
+                                : 'text-gray-300 hover:bg-gray-100',
+                            )}
+                            onClick={() => {
+                              setActiveDate(
+                                dayjs(day).startOf('month').toDate(),
+                              )
+                              setCalendarVisible(false)
+                              if (onChange) {
+                                onChange(day)
+                              }
+                            }}
+                          >
+                            {dayjs(day).format('DD')}
+                          </button>
+                        ))}
+                      </div>
+                    ))}
+                  </div>
                 </div>
-              </div>
-            </CSSTransition>
-          </SwitchTransition>
+              </CSSTransition>
+            </SwitchTransition>
+          </div>
+          <button
+            className="w-full px-4 py-2 text-sm font-medium text-center text-blue-600 rounded focus:outline-none focus:shadow-outline"
+            onClick={() => {
+              setActiveDate(new Date())
+            }}
+          >
+            Today
+          </button>
         </div>
-        <button
-          className="w-full px-4 py-2 text-sm font-medium text-center text-blue-600 rounded focus:outline-none focus:shadow-outline"
-          onClick={() => {
-            setActiveDate(new Date())
-          }}
-        >
-          Today
-        </button>
-      </div>
+      </Portal>
     </div>
   )
 }
