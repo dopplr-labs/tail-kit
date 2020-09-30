ee0c3a75ba96222a4e4d1f48767111ed9c6e8a15
hnagrath09 <him.nagrath@gmail.com>
2020-09-30 08:38:10 +0530

    Minor changes in JSX

--- src/components/select/select.tsx
+++ src/components/select/select.tsx
@@ -70,52 +70,50 @@ export function Select({
   })
 
   return (
-    <>
-      <div className="relative inline-block">
-        <button
-          className={clsx(
-            'flex px-3 py-2 items-center text-sm cursor-pointer justify-between overflow-hidden border rounded-md focus:shadow-outline focus:outline-none',
-            disabled
-              ? 'cursor-not-allowed bg-gray-100 text-gray-400'
-              : 'text-gray-800',
-            className,
-          )}
-          type="button"
-          disabled={disabled}
-          {...getToggleButtonProps()}
-        >
-          <span>{selectedItem ? itemToString(selectedItem) : placeholder}</span>
-          <ChevronDownOutline className="w-4 h-4" />
-        </button>
-        <ul
-          className="w-full mt-1 overflow-y-auto text-sm rounded-md shadow focus:outline-none"
-          {...getMenuProps()}
-        >
-          {isOpen &&
-            selectOptions.map((item, index) => (
-              <li
-                className={clsx(
-                  'px-3 py-2 flex items-center justify-between',
-                  highlightedIndex === index
-                    ? 'bg-blue-600 text-white'
-                    : undefined,
-                  selectedItem === item ? 'font-semibold' : undefined,
-                )}
-                key={`${item.label}${index}`}
-                {...getItemProps({ item: item, index })}
-              >
-                <div className="flex items-center space-x-2">
-                  <span>{item?.icon}</span>
-                  <span>{item.label}</span>
-                </div>
+    <div className="inline-block">
+      <button
+        className={clsx(
+          'flex px-3 py-2 items-center text-sm cursor-pointer justify-between overflow-hidden border rounded-md focus:shadow-outline focus:outline-none',
+          disabled
+            ? 'cursor-not-allowed bg-gray-100 text-gray-400'
+            : 'text-gray-800',
+          className,
+        )}
+        type="button"
+        disabled={disabled}
+        {...getToggleButtonProps()}
+      >
+        <span>{selectedItem ? itemToString(selectedItem) : placeholder}</span>
+        <ChevronDownOutline className="w-4 h-4" />
+      </button>
+      <ul
+        className="w-full mt-1 overflow-y-auto text-sm rounded-md shadow focus:outline-none"
+        {...getMenuProps()}
+      >
+        {isOpen &&
+          selectOptions.map((item, index) => (
+            <li
+              className={clsx(
+                'px-3 py-2 flex items-center justify-between',
+                highlightedIndex === index
+                  ? 'bg-blue-600 text-white'
+                  : undefined,
+                selectedItem === item ? 'font-semibold' : undefined,
+              )}
+              key={`${item.label}${index}`}
+              {...getItemProps({ item: item, index })}
+            >
+              <div className="flex items-center space-x-2">
+                <span>{item?.icon}</span>
+                <span>{item.label}</span>
+              </div>
 
-                {selectedItem === item ? (
-                  <CheckOutline className="w-5 h-5" />
-                ) : null}
-              </li>
-            ))}
-        </ul>
-      </div>
-    </>
+              {selectedItem === item ? (
+                <CheckOutline className="w-5 h-5" />
+              ) : null}
+            </li>
+          ))}
+      </ul>
+    </div>
   )
 }
