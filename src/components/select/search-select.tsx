bcb6b9901a0b29a65c1004f062b055b044ab0dee
hnagrath09 <him.nagrath@gmail.com>
2020-09-30 08:25:01 +0530

    Added new component search select

---
+++ src/components/select/search-select.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import { useCombobox } from 'downshift'
+import clsx from 'clsx'
+import Input from 'components/input'
+
+export type SearchSelectProps = {
+  options: string[]
+  placeholder: string
+}
+export function SearchSelect({ options, placeholder }: SearchSelectProps) {
+  const [inputItems, setInputItems] = useState(options)
+  const {
+    isOpen,
+    getMenuProps,
+    getInputProps,
+    getComboboxProps,
+    getToggleButtonProps,
+    highlightedIndex,
+    getItemProps,
+  } = useCombobox({
+    items: inputItems,
+    onInputValueChange: ({ inputValue }) => {
+      setInputItems(
+        options.filter((item) =>
+          item.toLowerCase().startsWith(inputValue?.toLowerCase() ?? ''),
+        ),
+      )
+    },
+  })
+
+  return (
+    <div className="inline-block">
+      <div {...getComboboxProps()}>
+        <Input
+          placeholder={placeholder}
+          {...getInputProps()}
+          {...getToggleButtonProps()}
+        />
+      </div>
+      <ul
+        className="w-full mt-1 overflow-y-auto text-sm rounded-md shadow focus:outline-none"
+        {...getMenuProps()}
+      >
+        {isOpen &&
+          inputItems.map((item, index) => (
+            <li
+              className={clsx(
+                'px-3 py-2 flex items-center justify-between',
+                highlightedIndex === index
+                  ? 'bg-blue-600 text-white'
+                  : undefined,
+              )}
+              key={`${item}${index}`}
+              {...getItemProps({ item, index })}
+            >
+              {item}
+            </li>
+          ))}
+      </ul>
+    </div>
+  )
+}
