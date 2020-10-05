26968bb2a2192752415f9bb5fd90916bdd27f3e9
Abinash Panda <abinash@prodios.com>
2020-10-05 23:27:23 +0530

    Update rollup configuration to generate type definitions

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import typescript from '@rollup/plugin-typescript'
+import typescript from 'rollup-plugin-typescript2'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import postcss from 'rollup-plugin-postcss'
