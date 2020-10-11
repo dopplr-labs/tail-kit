3a437c282e1c0098fbcc90e1440d840e880aa361
Himanshu Nagrath <him.nagrath@gmail.com>
2020-10-11 23:53:18 +0530

    Added story for radio group

--- src/components/radio/radio.stories.tsx
+++ src/components/radio/radio.stories.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
+import { action } from '@storybook/addon-actions'
 import { Meta } from '@storybook/react/types-6-0'
 import Switch from 'components/switch'
 import { Radio } from './radio'
+import { RadioGroup } from './radio-group'
 
 export default {
   title: 'Data Entry/Radio',
@@ -28,6 +30,33 @@ export function DisabledRadio() {
         />
         <span className="text-sm text-gray-800">Toggle Disabled Radio</span>
       </div>
+    </div>
+  )
+}
+
+export function WithRadioGroup() {
+  const plainOptions = ['Option A', 'Option B', 'Option C', 'Option D']
+  const options = [
+    { label: 'Alpha', value: '1' },
+    { label: 'Beta', value: '2' },
+    { label: 'Gama', value: '3' },
+    { label: 'Omega', value: '4' },
+  ]
+  const onChange = (RadioGroup: string) => (checkedValue: string) => {
+    action(`${RadioGroup}-onChange`)(checkedValue)
+  }
+  return (
+    <div className="space-y-5">
+      <RadioGroup
+        options={plainOptions}
+        defaultValue="Option A"
+        onChange={onChange('firstGroup')}
+      />
+      <RadioGroup
+        options={options}
+        defaultValue="3"
+        onChange={onChange('secondGroup')}
+      />
     </div>
   )
 }
