826dc47766b1db90177308c87525a67c43dd8b65
Abinash Panda <abinash@prodios.com>
2020-10-12 21:44:54 +0530

    Refactor Portal component to use usePortalPosition hook

--- .storybook/main.js
+++ .storybook/main.js
@@ -12,9 +12,9 @@ module.exports = {
 
     config.resolve.alias = {
       components: path.resolve(__dirname, '../src/components'),
-      hooks: path.resolve(__dirname, '../src/hooks'),
       styles: path.resolve(__dirname, '../src/styles'),
       hooks: path.resolve(__dirname, '../src/hooks'),
+      utils: path.resolve(__dirname, '../src/utils'),
     }
 
     config.module.rules.push({
--- types/components/portal/portal.d.ts
+++ types/components/portal/portal.d.ts
@@ -1,10 +1,18 @@
 import React from 'react'
-import { HorizontalPlacement, VerticalPlacement } from './utils'
+import { HorizontalPlacement, VerticalPlacement } from 'utils/portal'
 declare type PortalProps = {
+  /**
+   * Ref of the trigger element. The content container would render
+   * according the placement of the trigger
+   * */
   triggerRef: React.RefObject<HTMLElement | null>
+  /** Whether portal is visible or not */
   visible: boolean
+  /** Content to be rendered inside the portal */
   children: React.ReactElement
+  /** Default vertical placement. If provided, the portal won't calculate the vertical position */
   verticalPlacement?: VerticalPlacement
+  /** Default horizontal placement. If provided, the portal won't calculate the horizontal position */
   horizontalPlacement?: HorizontalPlacement
   /** parent of the portal container rendering the menu */
   portalParent?: HTMLElement
