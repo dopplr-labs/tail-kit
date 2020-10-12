9d3f49d823222747268b29cd0f23897d60fbe415
Himanshu Nagrath <him.nagrath@gmail.com>
2020-10-12 08:24:00 +0530

    Completed all test cases for radio component

--- src/components/radio/radio.test.tsx
+++ src/components/radio/radio.test.tsx
@@ -1,1 +1,29 @@
-test('Radio component renders correctly', () => {})
+import React from 'react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { Radio } from './radio'
+
+test('renders radio label correctly', () => {
+  render(<Radio label="Radio" />)
+  expect(screen.getByText('Radio')).toBeInTheDocument()
+})
+
+test('selection for radio working correctly', () => {
+  const onChange = jest.fn((event) => event.target.checked)
+  render(<Radio label="Radio" onChange={onChange} />)
+  fireEvent.click(screen.getByText('Radio'))
+  expect(onChange).toBeCalled()
+  expect(onChange.mock.results[0].value).toBe(true)
+})
+
+test('disable property of radio working correctly', () => {
+  render(<Radio disabled label="Radio" checked />)
+  expect(screen.getByText('Radio').parentElement).toHaveClass('text-gray-400')
+  expect(screen.getByText('Radio').parentElement?.firstChild).toHaveClass(
+    'border-gray-400',
+  )
+})
+
+test('renders radio without label correctly', () => {
+  render(<Radio />)
+  expect(screen.queryByTestId('label')).toBe(null)
+})
