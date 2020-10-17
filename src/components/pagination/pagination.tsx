02a3bfbc4f3d29bb29bcfc4d117545853341ae51
Himanshu Nagrath <him.nagrath@gmail.com>
2020-10-17 21:35:04 +0530

    Completed pagination component

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,23 +1,41 @@
-import React, { useState, useMemo, useEffect, useCallback } from 'react'
+import React, { useState, useMemo, useEffect, useCallback, useRef } from 'react'
+import clsx from 'clsx'
 import Button from 'components/button'
 import Select from 'components/select'
 import { ChevronLeftOutline, ChevronRightOutline } from 'components/icons'
+import useSyncedState from 'hooks/use-synced-states'
 import PageButton from './components/page-button'
+
+export type OnChangeType = (page: number, pageSize: number) => void
 
 /**
  * Pagination component properties
  */
 export type PaginationProps = {
+  /** Use current prop along with onChange
+   * to create controlled Pagination component
+   */
+  current?: number
   /** Default initial page number */
   defaultCurrent?: number
   /** Total number of data items */
   total: number
+  /** Number of total PageButtons displayed together  */
+  pageRangeDisplayed?: number
+  /** Fixed number of buttons on both side of Pagination component */
+  boundaryPageButtons?: number
   /** Determine whether to show pageSize select */
   showSizeChanger?: boolean
   /** Determine whether you can jump to pages directly */
   showQuickJumper?: boolean
   /** Specify the sizeChanger options */
   pageSizeOptions?: string[]
+  /** Called when the page number is changed, and it takes the resulting page number and pageSize as its arguments */
+  onChange?: OnChangeType
+  /** Additional classes applied to the Pagination component */
+  className?: string
+  /** Additional styles applied to the Pagination component */
+  style?: React.CSSProperties
 }
 
 /**
@@ -30,29 +48,52 @@ export type PaginationProps = {
  */
 
 export function Pagination({
+  current,
   defaultCurrent = 1,
   total,
+  pageRangeDisplayed = 5,
+  boundaryPageButtons = 2,
   showSizeChanger = false,
   showQuickJumper = false,
   pageSizeOptions = ['10', '20', '50', '100'],
+  onChange,
+  className,
+  style,
 }: PaginationProps) {
-  const [totalPages, setTotalPages] = useState<number>(
-    Math.ceil(total / parseFloat(pageSizeOptions[0])),
+  const [pageSize, setPageSize] = useSyncedState(pageSizeOptions[0])
+
+  const [selected, setSelected] = useSyncedState<number>(
+    current || defaultCurrent,
   )
-  const [selected, setSelected] = useState<number>(defaultCurrent)
   const [inputValue, setInputValue] = useState<string>('')
 
+  // Using useRef hook to avoid multiple onChange invokes in useEffect
+  const onChangeCb = useRef<OnChangeType | undefined>(undefined)
+  onChangeCb.current = onChange
+
+  const totalPages = Math.ceil(total / parseFloat(pageSize))
+
+  // Options to be used with Select component
   const options = useMemo(() => {
     return pageSizeOptions.map((size) => {
       return { label: `${size} / page`, value: size }
     })
   }, [pageSizeOptions])
 
+  /** After changing data per page if value of selected is greater than totalPages
+   *  then selected will be equal to totalPages
+   */
   useEffect(() => {
     if (selected > totalPages) {
       setSelected(totalPages)
     }
-  }, [selected, totalPages])
+  }, [selected, totalPages, setSelected])
+
+  /** Invoke onChange function everytime selected page or pageSize changes */
+  useEffect(() => {
+    const pageSizeValue = parseFloat(pageSize)
+    onChangeCb.current?.(selected, pageSizeValue)
+  }, [selected, pageSize])
 
   function handleDecrement() {
     setSelected((prevState) => prevState - 1)
@@ -63,8 +104,7 @@ export function Pagination({
   }
 
   function handlePageSize(selectedOption: string | undefined) {
-    const newPageSize = parseFloat(selectedOption ?? pageSizeOptions[0])
-    setTotalPages(Math.ceil(total / newPageSize))
+    setPageSize(selectedOption ?? pageSizeOptions[0])
   }
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
@@ -86,13 +126,15 @@ export function Pagination({
     setInputValue('')
   }
 
+  // Enter key shortcut used in pageJumper
   function handleEnterKeyPress(event: React.KeyboardEvent) {
     if (event.key === 'Enter') {
       handlePageJump()
     }
   }
 
-  const createPageView = useCallback(
+  // function returns single PageButton component
+  const createPageButton = useCallback(
     (page: number) => (
       <PageButton
         key={page}
@@ -101,60 +143,61 @@ export function Pagination({
         onClick={() => setSelected(page)}
       />
     ),
-    [selected],
+    [selected, setSelected],
   )
-
-  const MARGIN_PAGES_DISPLAYED = 2
-  const PAGE_RANGE_DISPLAYED = 5
 
   const renderButtons = useMemo(() => {
     const items = []
 
-    if (totalPages <= PAGE_RANGE_DISPLAYED) {
+    if (totalPages <= pageRangeDisplayed) {
       for (let page = 1; page <= totalPages; page++) {
-        items.push(createPageView(page))
+        items.push(createPageButton(page))
       }
     } else {
-      let leftSide = PAGE_RANGE_DISPLAYED / 2
-      let rightSide = PAGE_RANGE_DISPLAYED - leftSide
+      // Number of buttons to render on the left side of selected button
+      let leftSide = pageRangeDisplayed / 2
+
+      // Number of buttons to render on the right side of selected button
+      let rightSide = pageRangeDisplayed - leftSide
 
       // If the selected page index is on the default right side of the pagination,
       // we consider that the new right side is made up of it (= only one break element).
       // If the selected page index is on the default left side of the pagination,
       // we consider that the new left side is made up of it (= only one break element).
-      if (selected > totalPages - PAGE_RANGE_DISPLAYED / 2) {
+      if (selected > totalPages - pageRangeDisplayed / 2) {
         rightSide = totalPages - selected
-        leftSide = PAGE_RANGE_DISPLAYED - rightSide
-      } else if (selected < PAGE_RANGE_DISPLAYED / 2) {
+        leftSide = pageRangeDisplayed - rightSide
+      } else if (selected < pageRangeDisplayed / 2) {
         leftSide = selected
-        rightSide = PAGE_RANGE_DISPLAYED - leftSide
+        rightSide = pageRangeDisplayed - leftSide
       }
 
       let breakView
 
       for (let page = 1; page <= totalPages; page++) {
-        // If the page index is lower than the margin defined,
+        // If the page index is lower than
+        // the number of boundary buttons defined,
         // the page has to be displayed on the left side of
         // the pagination.
-        if (page <= MARGIN_PAGES_DISPLAYED) {
-          items.push(createPageView(page))
+        if (page <= boundaryPageButtons) {
+          items.push(createPageButton(page))
           continue
         }
 
-        // If the page index is greater than the page count
-        // minus the margin defined, the page has to be
-        // displayed on the right side of the pagination.
-        if (page > totalPages - MARGIN_PAGES_DISPLAYED) {
-          items.push(createPageView(page))
+        // If the page index is greater than the total pages
+        // minus the number of boundary buttons defined,
+        // the page has to be displayed on the right side of the pagination.
+        if (page > totalPages - boundaryPageButtons) {
+          items.push(createPageButton(page))
           continue
         }
 
         // If the page index is near the selected page index
-        // and inside the defined range (PAGE_RANDE_DISPLAYED)
+        // and inside the defined range (pageRangeDisplayed)
         // we have to display it (it will create the center
         // part of the pagination).
         if (page >= selected - leftSide && page <= selected + rightSide) {
-          items.push(createPageView(page))
+          items.push(createPageButton(page))
           continue
         }
 
@@ -173,10 +216,16 @@ export function Pagination({
       }
     }
     return items
-  }, [totalPages, createPageView, selected])
+  }, [
+    totalPages,
+    createPageButton,
+    selected,
+    boundaryPageButtons,
+    pageRangeDisplayed,
+  ])
 
   return (
-    <div className="flex items-center gap-x-2">
+    <div className={clsx('flex items-center gap-x-2', className)} style={style}>
       <Button
         label="Previous"
         buttonType={Button.ButtonType.link}
@@ -204,7 +253,7 @@ export function Pagination({
         <div className="flex items-center text-sm gap-x-2">
           <span>Go to</span>
           <input
-            className="w-16 px-3 py-1 border rounded-md focus:outline-none focus:shadow-outline"
+            className="w-16 px-3 py-2 border rounded-md focus:outline-none focus:shadow-outline"
             value={inputValue}
             onChange={handleChange}
             onBlur={handlePageJump}
