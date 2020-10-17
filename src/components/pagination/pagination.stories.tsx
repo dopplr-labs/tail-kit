f50c649a97d66a09456c42ff88d7a4f898521291
Himanshu Nagrath <him.nagrath@gmail.com>
2020-10-17 21:35:40 +0530

    Added story for controlled pagination component

--- src/components/pagination/pagination.stories.tsx
+++ src/components/pagination/pagination.stories.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Meta } from '@storybook/react/types-6-0'
+import { action } from '@storybook/addon-actions'
 import { Pagination } from './pagination'
 
 export default {
@@ -17,4 +18,24 @@ export function WithSizeChanger() {
 
 export function WithPageJumper() {
   return <Pagination total={500} showQuickJumper />
+}
+
+export function ControlledPagination() {
+  const [current, setCurrent] = useState(3)
+  return (
+    <div className="flex flex-col items-start gap-y-4">
+      <span className="ml-4 text-sm text-gray-800">
+        Selected Page: <b>{current}</b>
+      </span>
+      <Pagination
+        total={500}
+        current={current}
+        onChange={(page, pageSize) => {
+          setCurrent(page)
+          action('update')({ page, pageSize })
+        }}
+        showSizeChanger
+      />
+    </div>
+  )
 }
