58f1da0e7e0fff53d6a5cc05ce72d961533197c2
Abinash Panda <abinash@prodios.com>
2020-10-22 22:43:21 +0530

    Add startDate, endDate and disableDate props in date picker

--- src/components/date-picker/utils.ts
+++ src/components/date-picker/utils.ts
@@ -23,3 +23,44 @@ export function isDateEqual(
 
   return comparatorFunction(dateA) === comparatorFunction(dateB)
 }
+
+/**
+ * Returns the weeks for the month
+ *
+ * @param month The date (preferably start of date of the month) whose month weeks are to computed
+ */
+export function getWeeksForMonth(month: Date) {
+  const monthStartWeek = dayjs(month).clone().startOf('month').startOf('week')
+  const monthEndWeek = dayjs(month).clone().endOf('month').startOf('week')
+
+  const weeks = []
+  for (
+    let date = monthStartWeek;
+    date <= monthEndWeek;
+    date = dayjs(date).add(1, 'week')
+  ) {
+    weeks.push(date)
+  }
+
+  return weeks
+}
+
+export function isDateDisabled(
+  date: Date,
+  startDate?: Date,
+  endDate?: Date,
+  disableDate?: (date: Date) => boolean,
+) {
+  let isDisabled = false
+  if (!isDisabled && startDate) {
+    isDisabled = dayjs(date).startOf('day').isBefore(startDate)
+  }
+  if (!isDisabled && endDate) {
+    isDisabled = dayjs(date).startOf('day').isAfter(endDate)
+  }
+  if (!isDisabled && disableDate) {
+    isDisabled = disableDate(date)
+  }
+
+  return isDisabled
+}
