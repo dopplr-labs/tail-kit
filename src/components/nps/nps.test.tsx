058611d0991553370ebbb3bdbe9b03d2a301fd50
Himanshu Nagrath <him.nagrath@gmail.com>
2020-10-24 16:44:43 +0530

    completed test cases for nps component

--- src/components/nps/nps.test.tsx
+++ src/components/nps/nps.test.tsx
@@ -1,1 +1,37 @@
-test('NPSInput component working correctly', () => {})
+import React from 'react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { range } from 'lodash-es'
+import NPSInput from '.'
+
+test('NPSInput component working correctly', () => {
+  render(<NPSInput />)
+  expect(
+    screen.getByText(
+      'How likely are you to recommend us to your friends and colleagues?',
+    ),
+  )
+})
+
+test('onSubmit of NPSInput working correctly', () => {
+  const handleSubmit = jest.fn((score) => score)
+  render(<NPSInput onSubmit={handleSubmit} />)
+  fireEvent.click(screen.getByText('8'))
+  expect(handleSubmit).toBeCalled()
+  expect(handleSubmit.mock.results[0].value).toStrictEqual(8)
+})
+
+test('score highlighting correctly on mouse enter and mouse leave', () => {
+  render(<NPSInput />)
+  fireEvent.mouseEnter(screen.getByText('7'))
+  range(0, 11).forEach((score) => {
+    if (score < 8) {
+      expect(screen.getByText(score.toString())).toHaveClass('bg-blue-500')
+    } else {
+      expect(screen.getByText(score.toString())).toHaveClass('bg-gray-200')
+    }
+  })
+  fireEvent.mouseLeave(screen.getByText('7'))
+  range(0, 11).forEach((score) => {
+    expect(screen.getByText(score.toString())).toHaveClass('bg-gray-200')
+  })
+})
