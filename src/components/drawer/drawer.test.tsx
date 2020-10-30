8951ba420ad337d563b3bbc71a2f33802a4c3479
Himanshu Nagrath <him.nagrath@gmail.com>
2020-10-30 21:41:14 +0530

    added test cases for drawer component

--- src/components/drawer/drawer.test.tsx
+++ src/components/drawer/drawer.test.tsx
@@ -1,1 +1,66 @@
-test('drawer component working correctly', () => {})
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { Drawer } from './drawer'
+
+jest.mock('react-transition-group', () => {
+  const FakeTransition = jest.fn(({ children }) => children)
+  const FakeCSSTransition = jest.fn((props) => {
+    if (props.in && props.onEnter) {
+      props.onEnter()
+    } else if (!props.in && props.onExited) {
+      props.onExited()
+    }
+    return props.in ? <FakeTransition>{props.children}</FakeTransition> : null
+  })
+  return { CSSTransition: FakeCSSTransition, Transition: FakeTransition }
+})
+
+test('render content inside drawer correctly', () => {
+  render(
+    <Drawer visible>
+      <p>Some Contents...</p>
+    </Drawer>,
+  )
+  expect(screen.getByText('Some Contents...')).toBeInTheDocument()
+})
+
+test('render title correctly', () => {
+  render(
+    <Drawer visible title="Drawer Title">
+      <div />
+    </Drawer>,
+  )
+  expect(screen.getByText('Drawer Title')).toBeInTheDocument()
+})
+
+test('removes the drawer content from dom after unmount', () => {
+  render(
+    <Drawer visible={false}>
+      <p>Some Contents...</p>
+    </Drawer>,
+  )
+  expect(screen.queryByText('Some Contents...')).not.toBeInTheDocument()
+})
+
+test('closes the drawer on overlay click', () => {
+  const onRequestClose = jest.fn()
+  render(
+    <Drawer visible onRequestClose={onRequestClose}>
+      <p>Some Contents...</p>
+    </Drawer>,
+  )
+  userEvent.click(screen.getByTestId('drawer-overlay'))
+  expect(onRequestClose).toBeCalled()
+})
+
+test('close button of drawer working correctly', () => {
+  const onRequestClose = jest.fn()
+  render(
+    <Drawer visible onRequestClose={onRequestClose} closable>
+      <p>Some Contents...</p>
+    </Drawer>,
+  )
+  userEvent.click(screen.getByRole('button'))
+  expect(onRequestClose).toBeCalled()
+})
