9839d086b2366f77b20a29950419a505f33ba8d7
Himanshu Nagrath <him.nagrath@gmail.com>
2020-10-31 08:30:46 +0530

    Added test cases for drawer placement and footer

--- src/components/drawer/drawer.test.tsx
+++ src/components/drawer/drawer.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { Drawer } from './drawer'
+import Button from 'components/button'
+import { Drawer, DrawerPlacement } from './drawer'
 
 jest.mock('react-transition-group', () => {
   const FakeTransition = jest.fn(({ children }) => children)
@@ -63,4 +64,46 @@ test('close button of drawer working correctly', () => {
   )
   userEvent.click(screen.getByRole('button'))
   expect(onRequestClose).toBeCalled()
+})
+
+test('left placement drawer renders correctly', () => {
+  render(
+    <Drawer visible closable placement={DrawerPlacement.left}>
+      <p>Some Contents...</p>
+    </Drawer>,
+  )
+  expect(screen.getByRole('button').parentElement).toHaveClass(
+    'top-0 left-0 h-full',
+  )
+})
+
+test('top placement drawer renders correctly', () => {
+  render(
+    <Drawer visible closable placement={DrawerPlacement.top}>
+      <p>Some Contents...</p>
+    </Drawer>,
+  )
+  expect(screen.getByRole('button').parentElement).toHaveClass(
+    'top-0 left-0 w-full',
+  )
+})
+
+test('bottom placement drawer renders correctly', () => {
+  render(
+    <Drawer visible closable placement={DrawerPlacement.bottom}>
+      <p>Some Contents...</p>
+    </Drawer>,
+  )
+  expect(screen.getByRole('button').parentElement).toHaveClass(
+    'bottom-0 left-0 w-full',
+  )
+})
+
+test('footer inside drawer renders correctly', () => {
+  render(
+    <Drawer visible footer={<Button label="Cancel" />}>
+      <p>Some Contents...</p>
+    </Drawer>,
+  )
+  expect(screen.getByText('Cancel')).toBeInTheDocument()
 })
