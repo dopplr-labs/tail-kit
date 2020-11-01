330c2d7f0ba76a087a941a1ffa7dd7277f7a86ae
Abinash Panda <abinash@prodios.com>
2020-11-01 10:01:44 +0530

    Computed tooltip position based on content placement

--- src/components/tooltip/tooltip.stories.tsx
+++ src/components/tooltip/tooltip.stories.tsx
@@ -20,12 +20,8 @@ export function DefaultTooltip() {
 export function TooltipWithIcon() {
   return (
     <Tooltip
-      title={
-        <span className="text-red-500">
-          User once deleted cannot be restored
-        </span>
-      }
-      icon={<ExclamationOutline className="w-5 h-5 text-red-500" />}
+      title="User once deleted cannot be restored"
+      icon={<ExclamationOutline className="w-6 h-6 text-red-500" />}
     >
       <Button
         buttonType={Button.ButtonType.danger}
@@ -33,5 +29,42 @@ export function TooltipWithIcon() {
         label="Delete User"
       />
     </Tooltip>
+  )
+}
+
+export function TooltipWithExplicitPlacement() {
+  return (
+    <div className="flex items-center mx-auto my-20 space-x-2 w-min-content">
+      <Tooltip title="Left" defaultPlacement="left">
+        <Button label="Left" className="w-20" />
+      </Tooltip>
+      <div className="space-y-12">
+        <div className="flex space-x-4">
+          <Tooltip title="Top Left" defaultPlacement="topLeft">
+            <Button label="Top Left" className="w-32" />
+          </Tooltip>
+          <Tooltip title="Top" defaultPlacement="top">
+            <Button label="Top" className="w-32" />
+          </Tooltip>
+          <Tooltip title="Top Right" defaultPlacement="topRight">
+            <Button label="Top Right" className="w-32" />
+          </Tooltip>
+        </div>
+        <div className="flex space-x-4">
+          <Tooltip title="Bottom Left" defaultPlacement="bottomLeft">
+            <Button label="Bottom Left" className="w-32" />
+          </Tooltip>
+          <Tooltip title="Bottom" defaultPlacement="bottom">
+            <Button label="Bottom" className="w-32" />
+          </Tooltip>
+          <Tooltip title="Bottom Right" defaultPlacement="bottomRight">
+            <Button label="Bottom Right" className="w-32" />
+          </Tooltip>
+        </div>
+      </div>
+      <Tooltip title="Right" defaultPlacement="right">
+        <Button label="Right" className="w-20" />
+      </Tooltip>
+    </div>
   )
 }
--- src/components/tooltip/tooltip.tsx
+++ src/components/tooltip/tooltip.tsx
@@ -1,6 +1,20 @@
+import clsx from 'clsx'
 import Portal from 'components/portal'
 import React, { useRef, useState } from 'react'
 import { HorizontalPlacement, VerticalPlacement } from 'utils/portal'
+
+const TooltipPlacements: {
+  [key: string]: [VerticalPlacement, HorizontalPlacement]
+} = {
+  topLeft: [VerticalPlacement.top, HorizontalPlacement.leftAlign],
+  top: [VerticalPlacement.top, HorizontalPlacement.center],
+  topRight: [VerticalPlacement.top, HorizontalPlacement.rightAlign],
+  left: [VerticalPlacement.center, HorizontalPlacement.left],
+  right: [VerticalPlacement.center, HorizontalPlacement.right],
+  bottomLeft: [VerticalPlacement.bottom, HorizontalPlacement.leftAlign],
+  bottom: [VerticalPlacement.bottom, HorizontalPlacement.center],
+  bottomRight: [VerticalPlacement.bottom, HorizontalPlacement.rightAlign],
+}
 
 export type TooltipProps = {
   /** Title shown in the tooltip */
@@ -9,12 +23,18 @@ export type TooltipProps = {
   icon?: JSX.Element
   /** Content for which the tooltip is to be shown */
   children: React.ReactElement
+  defaultPlacement?: keyof typeof TooltipPlacements
 }
 
 /**
  * Component to show **tooltip**
  */
-export function Tooltip({ title, icon, children }: TooltipProps) {
+export function Tooltip({
+  title,
+  icon,
+  children,
+  defaultPlacement = 'right',
+}: TooltipProps) {
   const trigger = useRef<HTMLDivElement | null>(null)
 
   const [tooltipVisible, setTooltipVisible] = useState(false)
@@ -40,7 +60,6 @@ export function Tooltip({ title, icon, children }: TooltipProps) {
       <Portal
         triggerRef={trigger}
         visible={tooltipVisible}
-        // visible
         allowedPlacements={[
           [VerticalPlacement.top, HorizontalPlacement.leftAlign],
           [VerticalPlacement.top, HorizontalPlacement.center],
@@ -51,18 +70,45 @@ export function Tooltip({ title, icon, children }: TooltipProps) {
           [VerticalPlacement.bottom, HorizontalPlacement.center],
           [VerticalPlacement.bottom, HorizontalPlacement.rightAlign],
         ]}
-        defaultPlacement={[VerticalPlacement.center, HorizontalPlacement.right]}
+        defaultPlacement={TooltipPlacements[defaultPlacement]}
       >
-        {() => (
-          <div className="relative rounded-md shadow">
-            <div className="absolute left-0 w-2.5 h-2.5 transform rotate-45 -translate-x-1/2 -translate-y-1/2 bg-white shadow top-1/2" />
-            <div className="relative z-10 inline-flex px-4 py-2 space-x-3 text-sm text-gray-800 bg-white rounded-md">
-              {icon}
-              <span>{title}</span>
+        {({ containerPlacement }) => {
+          const [verticalPlacement, horizontalPlacement] = containerPlacement
+          return (
+            <div className="relative rounded-md shadow">
+              <div
+                className={clsx(
+                  'absolute w-2 h-2 transform rotate-45 bg-gray-700 shadow rounded-sm',
+                  verticalPlacement === VerticalPlacement.center
+                    ? 'top-1/2 -translate-y-1/2'
+                    : verticalPlacement === VerticalPlacement.top
+                    ? 'bottom-0 translate-y-1/2 mb-px'
+                    : verticalPlacement === VerticalPlacement.bottom
+                    ? 'top-0 -translate-y-1/2 mt-px'
+                    : undefined,
+                  horizontalPlacement === HorizontalPlacement.left
+                    ? 'right-0 translate-x-1/2 mr-px'
+                    : horizontalPlacement === HorizontalPlacement.right
+                    ? 'left-0 -translate-x-1/2 ml-px'
+                    : horizontalPlacement === HorizontalPlacement.leftAlign
+                    ? 'left-0 ml-4'
+                    : horizontalPlacement === HorizontalPlacement.rightAlign
+                    ? 'right-0 mr-4'
+                    : horizontalPlacement === HorizontalPlacement.center
+                    ? 'left-1/2 -translate-x-1/2'
+                    : undefined,
+                )}
+              />
+              <div className="relative z-10 inline-flex items-center px-4 py-2 space-x-3 text-sm text-white bg-gray-700 rounded-md">
+                {icon}
+                <span>{title}</span>
+              </div>
             </div>
-          </div>
-        )}
+          )
+        }}
       </Portal>
     </>
   )
 }
+
+Tooltip.Placements = TooltipPlacements
