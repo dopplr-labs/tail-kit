67cdc532518fa83058acd44f832f28e2d8293d13
Himanshu Nagrath <him.nagrath@gmail.com>
2020-11-02 14:59:50 +0530

    Added documentation for drawer component and exported in indexjs file

--- src/components/drawer/drawer.tsx
+++ src/components/drawer/drawer.tsx
@@ -42,6 +42,18 @@ export type DrawerProps = {
   style?: React.CSSProperties
 }
 
+/**
+ *  A panel which slides in from the edge of the screen.
+ *
+ * ### When To Use
+ *
+ * A Drawer is a panel that is typically overlaid on top of a page and slides in from the side. It contains a set of information or actions. Since the user can interact with the Drawer without leaving the current page, tasks can be achieved more efficiently within the same context.
+ *
+ * * Use a Form to create or edit a set of information.
+ * * Processing subtasks. When subtasks are too heavy for a Popover and we still want to keep the subtasks in the context of the main task, Drawer comes very handy.
+ * * When the same Form is needed in multiple places.
+ */
+
 export function Drawer({
   children,
   closable = false,
