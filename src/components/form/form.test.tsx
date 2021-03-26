47c98e5941828cdfd6adb23e8c294a158468ee5f
Himanshu Nagrath <him.nagrath@gmail.com>
2021-03-26 20:30:28 +0530

    test(form): added more test cases for form

--- src/components/form/form.test.tsx
+++ src/components/form/form.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import Input from 'components/input'
+import Button from 'components/button'
 import { Form } from './form'
 
 test('render form correctly', () => {
@@ -12,4 +13,49 @@ test('render form correctly', () => {
     </Form>,
   )
   expect(screen.getByText('Email :')).toBeInTheDocument()
+})
+
+test('inline layout form renders correctly', () => {
+  render(
+    <Form layout={Form.Layout.INLINE}>
+      <Form.Item name="email" label="Email">
+        <Input />
+      </Form.Item>
+    </Form>,
+  )
+  expect(screen.getByText('Email :').parentElement).toHaveClass(
+    'flex items-center',
+  )
+})
+
+test('vertical layout form renders correctly', () => {
+  render(
+    <Form layout={Form.Layout.VERTICAL}>
+      <Form.Item name="email" label="Email">
+        <Input />
+      </Form.Item>
+    </Form>,
+  )
+  expect(screen.getByText('Email').parentElement).toHaveClass('flex flex-col')
+})
+
+test('error message renders correctly', async () => {
+  const onSubmit = jest.fn((data) => data)
+  render(
+    <Form onSubmit={onSubmit}>
+      <Form.Item
+        name="email"
+        rules={[{ required: true, message: 'Please enter your email address' }]}
+      >
+        <Input />
+      </Form.Item>
+      <Button type="submit" label="Submit" />
+    </Form>,
+  )
+  fireEvent.click(screen.getByText('Submit'))
+  await waitFor(() => {
+    expect(
+      screen.getByText('Please enter your email address'),
+    ).toBeInTheDocument()
+  })
 })
