dacf141a3f089a719c85ae224c3fe7b412fd0f59
Himanshu Nagrath <him.nagrath@gmail.com>
2021-05-07 23:42:14 +0530

    feat(avatar): added maxCount and maxStyle props in AvatarGroup

--- src/components/avatar/avatar-group.tsx
+++ src/components/avatar/avatar-group.tsx
@@ -1,4 +1,6 @@
 import React, { cloneElement } from 'react'
+import Popover from 'components/popover'
+import Avatar from '.'
 
 /**
  * AvatarGroup Properties
@@ -6,12 +8,19 @@ import React, { cloneElement } from 'react'
 export type AvatarGroupProps = {
   children: React.ReactNode
   maxCount?: number
+  maxStyle?: React.CSSProperties
 }
 
-export function AvatarGroup({ children, maxCount }: AvatarGroupProps) {
+export function AvatarGroup({
+  children,
+  maxCount,
+  maxStyle,
+}: AvatarGroupProps) {
   if (maxCount && React.Children.count(children) > maxCount) {
     const childrenArr = React.Children.toArray(children)
     const childrenShow = childrenArr.slice(0, maxCount)
+    const childrenHide = childrenArr.slice(maxCount)
+    const extraChildren = React.Children.count(children) - maxCount
     return (
       <div className="flex items-center">
         {React.Children.map(childrenShow, (child) =>
@@ -19,6 +28,18 @@ export function AvatarGroup({ children, maxCount }: AvatarGroupProps) {
             className: '-ml-2 border border-white',
           }),
         )}
+        <Popover
+          content={
+            <div className="flex items-center space-x-1">{childrenHide}</div>
+          }
+        >
+          <Avatar
+            className="-ml-2 text-sm border border-white"
+            style={maxStyle}
+          >
+            {`+${extraChildren.toString()}`}
+          </Avatar>
+        </Popover>
       </div>
     )
   }
