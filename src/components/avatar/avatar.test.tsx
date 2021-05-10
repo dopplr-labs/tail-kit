56f3cea33d0481842a18cede9691c5639b5e807c
Himanshu Nagrath <him.nagrath@gmail.com>
2021-05-10 10:49:34 +0530

    test(avatar): added test cases and docs for avatar

--- src/components/avatar/avatar.test.tsx
+++ src/components/avatar/avatar.test.tsx
@@ -46,27 +46,9 @@ test('render avatar with square shape correctly', () => {
   ).toBe(false)
 })
 
-// Write testing logics for below
 test('render icon in avatar working correctly', () => {
-  render(<Avatar icon={<AiOutlineDown />} />)
-})
-
-test('render blank avatar correctly', () => {
-  render(<Avatar />)
-})
-
-test('render avatar-group correctly', () => {
-  render(
-    <Avatar.Group>
-      <Avatar src="https://randomuser.me/api/portraits/thumb/men/75.jpg" />
-      <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
-      <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutline />} />
-      <Avatar
-        style={{ backgroundColor: '#1890ff' }}
-        icon={<AnnotationSolid />}
-      />
-    </Avatar.Group>,
-  )
+  render(<Avatar icon={<AiOutlineDown title="Down" />} />)
+  expect(screen.getByTitle('Down')).toBeInTheDocument()
 })
 
 test('render avatar-group with few hidden avatars correctly', () => {
@@ -82,6 +64,26 @@ test('render avatar-group with few hidden avatars correctly', () => {
       <Tooltip title="Brad Gibson" placement="bottom">
         <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutline />} />
       </Tooltip>
+      <Avatar
+        style={{ backgroundColor: '#1890ff' }}
+        icon={<AnnotationSolid />}
+      />
+    </Avatar.Group>,
+  )
+  expect(screen.getByText('+2')).toBeInTheDocument()
+})
+
+// Write testing logics for below
+test('render blank avatar correctly', () => {
+  render(<Avatar />)
+})
+
+test('render avatar-group correctly', () => {
+  render(
+    <Avatar.Group>
+      <Avatar src="https://randomuser.me/api/portraits/thumb/men/75.jpg" />
+      <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
+      <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutline />} />
       <Avatar
         style={{ backgroundColor: '#1890ff' }}
         icon={<AnnotationSolid />}
