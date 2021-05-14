76b981c376ad732fb4c31b633648d80d38ebea64
Himanshu Nagrath <him.nagrath@gmail.com>
2021-05-14 20:54:03 +0530

    fix(avatar): resolved issues raised in PR review

--- src/components/avatar/avatar-group.tsx
+++ src/components/avatar/avatar-group.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import clsx from 'clsx'
 import Popover from 'components/popover'
 import Avatar from '.'
 import AvatarContext from './avatar-context'
@@ -9,33 +10,38 @@ import AvatarContext from './avatar-context'
 export type AvatarGroupProps = {
   /** Render Avatars in AvatarGroup as children */
   children: React.ReactNode
+  /** Customize style of excess avatar */
+  excessAvatarClassName?: string
+  /** Customize style of excess avatar */
+  excessAvatarStyle?: React.CSSProperties
   /** Maximum number of Avatars to show */
   maxCount?: number
   /** The placement of excess avatar Popover */
   maxPopoverPlacement?: 'top' | 'bottom'
-  /** Customize style of excess avatar */
-  maxStyle?: React.CSSProperties
   /** The size of the avatar */
   size?: 'large' | 'default' | 'small'
 }
 
 export function AvatarGroup({
   children,
+  excessAvatarClassName,
+  excessAvatarStyle,
   maxCount,
   maxPopoverPlacement = 'top',
-  maxStyle,
   size = 'default',
 }: AvatarGroupProps) {
-  if (maxCount && React.Children.count(children) > maxCount) {
+  const childrenCount = React.Children.count(children)
+
+  if (maxCount && childrenCount > maxCount) {
     const childrenArr = React.Children.toArray(children)
     const childrenShow = childrenArr.slice(0, maxCount)
     const childrenHide = childrenArr.slice(maxCount)
-    const extraChildren = React.Children.count(children) - maxCount
+    const extraChildren = childrenCount - maxCount
     return (
       <AvatarContext.Provider value={{ size }}>
         <div className="flex items-center">
-          {React.Children.map(childrenShow, (child) => (
-            <div className="-ml-2 border border-white rounded-full">
+          {React.Children.map(childrenShow, (child, index) => (
+            <div key={index} className="-ml-2 border border-white rounded-full">
               {child}
             </div>
           ))}
@@ -46,10 +52,13 @@ export function AvatarGroup({
             placement={maxPopoverPlacement}
           >
             <Avatar
-              className="-ml-2 text-sm border border-white"
-              style={maxStyle}
+              className={clsx(
+                '-ml-2 text-sm border border-white',
+                excessAvatarClassName,
+              )}
+              style={excessAvatarStyle}
             >
-              {`+${extraChildren.toString()}`}
+              {`+${extraChildren}`}
             </Avatar>
           </Popover>
         </div>
@@ -60,8 +69,11 @@ export function AvatarGroup({
   return (
     <AvatarContext.Provider value={{ size }}>
       <div className="flex items-center">
-        {React.Children.map(children, (child) => (
-          <div className="inline-block -ml-2 border border-white rounded-full">
+        {React.Children.map(children, (child, index) => (
+          <div
+            key={index}
+            className="inline-block -ml-2 border border-white rounded-full"
+          >
             {child}
           </div>
         ))}
