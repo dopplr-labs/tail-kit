bb56a3c81a1e60b4ac8b091a88dd3f34658211fa
Himanshu Nagrath <him.nagrath@gmail.com>
2021-05-14 20:54:03 +0530

    fix(avatar): resolved issues raised in PR review

--- types/components/avatar/avatar-group.d.ts
+++ types/components/avatar/avatar-group.d.ts
@@ -5,19 +5,22 @@ import React from 'react'
 export declare type AvatarGroupProps = {
   /** Render Avatars in AvatarGroup as children */
   children: React.ReactNode
+  /** Customize style of excess avatar */
+  excessAvatarClassName?: string
+  /** Customize style of excess avatar */
+  excessAvatarStyle?: React.CSSProperties
   /** Maximum number of Avatars to show */
   maxCount?: number
   /** The placement of excess avatar Popover */
   maxPopoverPlacement?: 'top' | 'bottom'
-  /** Customize style of excess avatar */
-  maxStyle?: React.CSSProperties
   /** The size of the avatar */
   size?: 'large' | 'default' | 'small'
 }
 export declare function AvatarGroup({
   children,
+  excessAvatarClassName,
+  excessAvatarStyle,
   maxCount,
   maxPopoverPlacement,
-  maxStyle,
   size,
 }: AvatarGroupProps): JSX.Element
