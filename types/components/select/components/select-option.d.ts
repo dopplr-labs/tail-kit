c026b3ebf2264309d05191504cf4d8bf679f4d54
Himanshu Nagrath <him.nagrath@gmail.com>
2021-07-18 11:55:19 +0530

    fix(rebase): rebase with develop and resolved conflicts

--- types/components/select/components/select-option.d.ts
+++ types/components/select/components/select-option.d.ts
@@ -1,9 +1,19 @@
-import React from 'react';
-import { OptionType } from '../types';
-declare type SelectOptionProps = Omit<React.DetailedHTMLProps<React.LiHTMLAttributes<HTMLLIElement>, HTMLLIElement>, 'children' | 'tabIndex'> & {
-    option: OptionType;
-    highlighted?: boolean;
-    selected?: boolean;
-};
-export declare function SelectOption({ option, highlighted, selected, className, style, ...restProps }: SelectOptionProps): JSX.Element;
-export {};
+import React from 'react'
+import { OptionType } from '../types'
+declare type SelectOptionProps = Omit<
+  React.DetailedHTMLProps<React.LiHTMLAttributes<HTMLLIElement>, HTMLLIElement>,
+  'children' | 'tabIndex'
+> & {
+  option: OptionType
+  highlighted?: boolean
+  selected?: boolean
+}
+export declare function SelectOption({
+  option,
+  highlighted,
+  selected,
+  className,
+  style,
+  ...restProps
+}: SelectOptionProps): JSX.Element
+export {}
--- types/hooks/use-synced-states/use-synced-states.d.ts
+++ types/hooks/use-synced-states/use-synced-states.d.ts
@@ -1,2 +1,4 @@
-import React from 'react';
-export declare function useSyncedState<T extends any>(prop: T): [T, React.Dispatch<React.SetStateAction<T>>];
+import React from 'react'
+export declare function useSyncedState<T extends any>(
+  prop: T,
+): [T, React.Dispatch<React.SetStateAction<T>>]
