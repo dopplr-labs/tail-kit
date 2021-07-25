332ea51d3e64a17a99ac23559b447134b1ba51a8
Himanshu Nagrath <him.nagrath@gmail.com>
2021-07-25 14:56:39 +0530

    added dynamic html title for docs

--- website/src/gatsby-theme-docz/wrapper.js
+++ website/src/gatsby-theme-docz/wrapper.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Helmet } from 'react-helmet-async'
+import { useCurrentDoc } from 'docz'
 
 // The doc prop contains some metadata about the page being rendered that you can use.
 const Wrapper = ({ children }) => {
+  const currentDoc = useCurrentDoc()
   return (
     <React.Fragment>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>tail-kit docs</title>
+        <title>{currentDoc.name} | tail-kit docs</title>
         <link
           rel="icon"
           type="image/ico"
