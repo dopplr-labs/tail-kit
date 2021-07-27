8b45dff8ff32b0867ccae92fdc0826651acc72d9
Abinash Panda <abinash@prodios.com>
2021-07-27 20:46:08 +0530

    feat(next-docs): add favicon and title

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 import { AppProps } from 'next/app'
 import '@tail-kit/tail-kit/dist/tail-kit.css'
 import 'styles/index.css'
+import Head from 'next/head'
 
 export default function DocsApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/tail-kit-logo.png" type="image/png" />
+        <title>Tail Kit Documentation</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
