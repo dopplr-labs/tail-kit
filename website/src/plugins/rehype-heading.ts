f0c85a1a61bbdba5a2c001eeff51db0d1ded9cc3
Abinash Panda <abinash@prodios.com>
2021-07-27 20:45:13 +0530

    feat(next-docs): add documetation for rehype plugins

--- website/src/plugins/rehype-heading.ts
+++ website/src/plugins/rehype-heading.ts
@@ -8,14 +8,29 @@ export type HeadingNode = {
   children: HeadingNode[]
 }
 
+/**
+ * Rehype plugin to get all the headings with their content and slug. This plugin also retains the heirarchy of headings.
+ * For example, if you have a heading(h2) with a subheading(h3), the subheading will be a child of the heading.
+ * Right now, this only works for h2 and h3 headings.
+ *
+ * @param headingsContainer - the list in which all the headings would be populated
+ * @returns - a transformer function which would traverse the AST and inject all the headings found
+ */
 export default function rehypeHeading(headingsContainer: HeadingNode[]) {
   return () => (tree: any) => {
+    // node containing the latest heading (h2)
     let currentHeading: HeadingNode | undefined
+
+    // visit all the elements in the AST
     visit(tree, 'element', (node: any) => {
       if (node.tagName === 'h2') {
+        // if the current element is heading and there is another heading element
+        // already present, then we need to first push the previous heading-subheading heirarchy into the
+        // headingsContainer and then create a new heading-subheading heirarchy
         if (currentHeading) {
           headingsContainer.push(currentHeading)
         }
+
         currentHeading = {
           type: 'h2',
           name: nodeToString(node),
@@ -29,13 +44,16 @@ export default function rehypeHeading(headingsContainer: HeadingNode[]) {
           slug: node.properties.id,
           children: [],
         }
+        // if a heading node is already present, then it means this is child of heading
         if (currentHeading) {
           currentHeading.children.push(heading)
         } else {
+          // it is an independent heading
           headingsContainer.push(heading)
         }
       }
     })
+    // for the last heading, push it into the headingsContainer
     if (currentHeading) {
       headingsContainer.push(currentHeading)
     }
--- website/src/plugins/rehype-playground.ts
+++ website/src/plugins/rehype-playground.ts
@@ -2,23 +2,38 @@ import toMarkdown from 'mdast-util-to-markdown'
 import mdxJsx from 'mdast-util-mdx-jsx'
 import prettier from 'prettier'
 
+/**
+ * Rehype plugin to inject code prop into the Playground component.
+ * The Playground component expects the code to be a string, but in the mdx files
+ * we tend to write the code as a JSX element. So this plugins transforms the JSX into
+ * string and inserts it into the code prop.
+ *
+ * @returns - a transformer function which would traverse the AST and udpate it
+ */
 export default function rehypePlayground() {
   return (tree: any) => {
+    // get all the Playground components from ast
     const playgrounds = tree.children.filter(
       (node: any) =>
         node.type === 'mdxJsxFlowElement' && node.name === 'Playground',
     )
+
     for (const playground of playgrounds) {
       const children = playground.children?.[0]
+
       if (!children) {
         return
       }
+
       let code: string | undefined
+
+      // if child is another JSX element, we need to convert it to markdown string
       if (children.type === 'mdxJsxFlowElement') {
         code = toMarkdown(children, {
           extensions: [mdxJsx.toMarkdown],
         })
       } else if (children.type === 'mdxFlowExpression') {
+        // else if the child is a string, we just use the formatted string
         code = prettier.format(children.value, {
           semi: false,
           trailingComma: 'all',
@@ -27,6 +42,9 @@ export default function rehypePlayground() {
           parser: 'babel',
         })
       }
+
+      // inject the code into the Playground component's attributes
+      // attributes correspond to the props in mdx
       if (code && Array.isArray(playground.attributes)) {
         playground.attributes.push({
           type: 'mdxJsxAttribute',
