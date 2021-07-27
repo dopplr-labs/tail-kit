ced2b811c303f44f8da054ec3b4b1e9643f78bea
Abinash Panda <abinash@prodios.com>
2021-07-27 20:45:46 +0530

    feat(next-docs): add editable prop in the Playground component

--- website/src/components/playground/playground.tsx
+++ website/src/components/playground/playground.tsx
@@ -1,15 +1,22 @@
 import React, { useRef, useState } from 'react'
 import { LiveProvider, LivePreview, LiveEditor, LiveError } from 'react-live'
-import * as TailKit from '@tail-kit/tail-kit'
+import { HiPencil } from 'react-icons/hi'
+import * as tailKit from '@tail-kit/tail-kit'
 import theme from 'prism-react-renderer/themes/nightOwl'
 import copy from 'copy-to-clipboard'
+import clsx from 'clsx'
 
 type PlaygroundProps = {
   code?: string
   scope?: { [key: string]: any }
+  editable?: boolean
 }
 
-export function Playground({ code, scope = {} }: PlaygroundProps) {
+export function Playground({
+  code,
+  scope = {},
+  editable = false,
+}: PlaygroundProps) {
   const [codeVisible, setCodeVisible] = useState(true)
   const [codeCopied, setCodeCopied] = useState(false)
   const timeout = useRef(null)
@@ -21,13 +28,19 @@ export function Playground({ code, scope = {} }: PlaygroundProps) {
   return (
     <LiveProvider
       code={code.trim()}
-      scope={{ ...TailKit, ...scope }}
+      scope={{
+        // pass tail-kit as the scope as all the components present in tail-kit would be somewhere or the used in the playground
+        ...tailKit,
+        ...scope,
+      }}
       theme={theme}
     >
       <div>
         <div className="border rounded-md">
           <LivePreview className="relative p-4" />
+
           <div className="relative">
+            {/* Toolbar */}
             <div className="absolute top-0 z-20 flex items-center justify-end w-full px-4 space-x-4">
               {codeVisible ? (
                 <button
@@ -57,14 +70,26 @@ export function Playground({ code, scope = {} }: PlaygroundProps) {
                 {codeVisible ? 'Hide' : 'Show'} Code
               </button>
             </div>
+
             {codeVisible ? (
               <LiveEditor
-                className="!font-mono text-sm leading-6 rounded-b-md live-editor"
-                disabled
+                className={clsx(
+                  '!font-mono text-sm leading-6 live-editor',
+                  !editable ? 'rounded-b-md' : undefined,
+                )}
+                disabled={!editable}
               />
+            ) : null}
+
+            {editable ? (
+              <div className="flex items-center px-4 py-2 space-x-2 text-xs font-medium bg-gray-100">
+                <HiPencil size={16} />
+                <span>Editable Example</span>
+              </div>
             ) : null}
           </div>
         </div>
+
         <LiveError className="px-4 py-2 mt-2 text-xs text-red-500 rounded-md bg-red-50" />
       </div>
     </LiveProvider>
