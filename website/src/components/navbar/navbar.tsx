dbd6626bc9f6cd8baf93f32c090d21c1f5e2f878
Himanshu Nagrath <him.nagrath@gmail.com>
2021-07-29 12:39:20 +0530

    feat(navbar): created navbar for website

---
+++ website/src/components/navbar/navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { FaGithub } from 'react-icons/fa'
+
+export default function Navbar() {
+  return (
+    <nav
+      id="navbar"
+      className="sticky top-0 z-40 flex items-center flex-none w-full mx-auto bg-white border-b lg:z-50 max-w-8xl"
+    >
+      <div className="flex-shrink-0 px-6 py-3 xl:px-8 xl:w-72 lg:w-64">
+        <img src="brand-image.png" className="w-24" />
+      </div>
+      <div className="flex justify-between flex-auto px-6 py-3">
+        <div className="flex-1" />
+        <a
+          href="https://github.com/dopplr-labs/tail-kit"
+          target="_blank"
+          rel="noopenner noreferrer"
+          className="focus:outline-none"
+        >
+          <FaGithub className="w-5 h-5 text-gray-500 hover:text-gray-700" />
+        </a>
+      </div>
+    </nav>
+  )
+}
