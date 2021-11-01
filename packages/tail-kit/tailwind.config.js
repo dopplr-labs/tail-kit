2b4d9aeda47faad774914ddae4e9a4f457da1b38
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2021-11-01 19:10:41 +0530

    created css variable to enable theming in button

--- packages/tail-kit/tailwind.config.js
+++ packages/tail-kit/tailwind.config.js
@@ -1,4 +1,27 @@
 module.exports = {
   presets: [require('../../tailwind.config')],
   purge: ['./src/**/*.tsx', './src/**/*.ts', './src/styles/safelist.txt'],
+  theme: {
+    extend: {
+      fontFamily: {
+        default: 'var(--font-family)',
+      },
+      colors: {
+        primary: 'var(--primary-color)',
+        error: 'var(--error-color)',
+        disabled: 'var(--disabled-color)',
+        text: {
+          primary: 'var(--text-primary-color)',
+          inverse: 'var(--text-inverse-color)',
+        },
+      },
+      borderRadius: {
+        default: 'var(--border-radius-default)',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
